1af8447700aa2c28a3f1744e3c609ab7be99b9bd
吴震宇 <wu.zhenyu@darcytech.com>
2021-05-06 15:09:22 +0800

    fix(eslint-config-base-preset): 解决import/extensions 规则配置错误问题

--- packages/eslint-config-base-preset/lib/eslint-config-base-preset.js
+++ packages/eslint-config-base-preset/lib/eslint-config-base-preset.js
@@ -1,11 +1,20 @@
 module.exports = {
-  root: true,
   env: { es2021: true, jest: true, node: true },
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.json',
+  },
+  settings: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
   extends: ['airbnb-typescript/base', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
   plugins: ['jest'],
