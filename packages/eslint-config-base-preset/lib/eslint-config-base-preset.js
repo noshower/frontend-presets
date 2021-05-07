bb29ecf7f03514287437eda9f13d9a642cff5bf5
吴震宇 <wu.zhenyu@darcytech.com>
2021-05-07 10:44:13 +0800

    fix(eslint-config-base-preset): 删除 import/extensions 的配置

--- packages/eslint-config-base-preset/lib/eslint-config-base-preset.js
+++ packages/eslint-config-base-preset/lib/eslint-config-base-preset.js
@@ -1,20 +1,13 @@
 module.exports = {
-  env: { es2021: true, jest: true, node: true },
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
-  },
-  settings: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
   },
   extends: ['airbnb-typescript/base', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
   plugins: ['jest'],
